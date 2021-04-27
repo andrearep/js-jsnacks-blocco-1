30f6e6291bb5a87c4cbda74e0ad4718c4d323af2
andrearep <andrea.repe@gmail.com>
2021-04-27 10:58:03 +0200

    snack completato

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ var word2 = prompt("inserire primo testo");
 if (word1.length === word2.length) {
     console.log("hanno la stessa lungheza");
 } else if (word1.length > word2.length) {
+    console.log(word2);
     console.log(word1);
 } else if (word1.length < word2.length) {
+    console.log(word1);
     console.log(word2);
 }
