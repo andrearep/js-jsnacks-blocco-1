7cc159fd7e3289301e5f78c9d2ee0dc3ccf1e843
andrearep <andrea.repe@gmail.com>
2021-05-10 12:12:37 +0200

    aggiunto tofixed

--- blocco_4/main.js
+++ blocco_4/main.js
@@ -14,9 +14,8 @@ console.log(palla.peso); */
 var triangolo = {
     h: 10,
     b: 15,
-
 }
 var c = Math.sqrt(triangolo.h ** 2 + triangolo.b ** 2)
 var perimetro = triangolo.h + triangolo.b + c;
 var area = (triangolo.b * triangolo.h) / 2;
-console.log("l'area è : " + area + " il perimetro è: " + perimetro);
+console.log("l'area è : " + area + " il perimetro è: " + perimetro.toFixed(2));
