512560461e7723aa32712d9a828a3982a9164be4
andrearep <andrea.repe@gmail.com>
2021-05-10 12:10:25 +0200

    calcolo area e perimetro del triangolo

--- blocco_4/main.js
+++ blocco_4/main.js
@@ -1,4 +1,4 @@
-var palla = {
+/* var palla = {
     nome: "palla",
     peso: 10
 }
@@ -9,4 +9,14 @@ do {
 
 
 palla.peso = pesoUtente;
-console.log(palla.peso);
+console.log(palla.peso); */
+
+var triangolo = {
+    h: 10,
+    b: 15,
+
+}
+var c = Math.sqrt(triangolo.h ** 2 + triangolo.b ** 2)
+var perimetro = triangolo.h + triangolo.b + c;
+var area = (triangolo.b * triangolo.h) / 2;
+console.log("l'area è : " + area + " il perimetro è: " + perimetro);
